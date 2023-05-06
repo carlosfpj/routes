5a5d680f10c2b0d9baff42a0fe7ff14e3a43ad18
carlosfpj <cafer89@gmail.com>
2023-05-06 16:22:11 -0500

    add blogpost route, outlet component used

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {HashRouter, Routes, Route} from "react-router-dom";
 import { HomePage } from "./HomePage";
 import {ProfilePage} from "./ProfilePage";
 import {BlogPage} from "./BlogPage";
+import { BlogPost } from "./Blogpost";
 import { Menu } from "./Menu";
 
 function App() {
@@ -11,7 +12,9 @@ function App() {
         <Menu/>
         <Routes>
         <Route path="/" element={<HomePage/>}/>
-          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/blog" element={<BlogPage />}>
+            <Route path=":slug" element={<BlogPost />} />
+          </Route>
           <Route path="/profile" element={<ProfilePage />} />
           <Route path="*" element={<p>not found</p>} />
         </Routes>
---
+++ src/blogdata.js
@@ -0,0 +1,24 @@
+const blogdata = [];
+
+blogdata.push({
+  title: 'primer titulo',
+  slug: 'primer-slug',
+  author: 'CFPJ',
+  contenido: 'primer contenido'
+})
+
+blogdata.push({
+  title: 'segundo titulo',
+  slug: 'segundo-slug',
+  author: 'NJR',
+  contenido: 'segundo contenido'
+})
+
+blogdata.push({
+  title: 'tercer titulo',
+  slug: 'tercer-slug',
+  author: 'MDPC',
+  contenido: 'tercer contenido'
+})
+
+export {blogdata}
